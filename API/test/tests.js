cf898580c8f7d5107de2b8864451aa74c9ca05dd
umar Abubukar <aminualee02@hotmail.com>
2019-06-17 10:25:09 +0200

    feat(update order): updare purchase order [Finishes #166726242]

--- API/test/tests.js
+++ API/test/tests.js
@@ -1,11 +1,9 @@
-var expect  = require('chai').expect;
-var chai = require('chai');
-var chaiHttp = require('chai-http');
-
-
+import chai from 'chai';
+import chaiHttp from 'chai-Http';
 
 chai.use(chaiHttp);
-chai.should();
+var expect = chai.expect;
+var should = chai.should();
 
 describe('Car Test', function() 
 {
@@ -33,4 +31,21 @@ describe('Car Test', function()
         done();    
         });
     })
+});
+describe('Order Test', function() {
+
+    describe('PATCH request: should be able to update order price', function () {
+        const order_id = 5,  new_price = 3000.00;
+        it('should be able to update order price', (done) => {
+            chai.request('http://localhost:5000/')
+            .patch(`api/v1/order/${ order_id }/price`)
+            .end((err, res) => {
+                expect(res.statusCode).to.equal(200);  
+                res.body.should.be.a('object');
+                res.body.should.have.property('price_offered').eql(new_price);
+            });
+            done();
+        })
+    })
+
 });
